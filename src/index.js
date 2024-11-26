4fab6242d8bd1f3ffd1973d630866cdbd9e3e4ee
gohnzy <gregory.nuzzo@gmail.com>
2024-11-26 13:20:17 +0100

    maintenance mode

--- src/index.js
+++ src/index.js
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Homepage from './pages/Homepage';
+import Maintenance from './pages/Maintenance';
 
 import './assets/styles/index.css';
+import axios from 'axios';
+
+const Middleware = () => {
+	const [maintenanceState, setMaintenanceState] = useState(null);
+
+	useEffect(() => {
+		const checkStatus = async () => {
+			try {
+				const response = await axios.get('/api');
+				if (response.status !== 503) {
+					setMaintenanceState(false);
+				}
+			} catch (error) {
+				if (error.response && error.response.status === 503) {
+					setMaintenanceState(true);
+				} else {
+					console.error(
+						'Error occured while checking for maintenance state',
+						error,
+					);
+					setMaintenanceState(false);
+				}
+			}
+		};
+	});
+	return maintenanceState ? <Maintenance /> : <Homepage />;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<Router>
 			<Routes>
-				<Route path="/" element={<Homepage />} />
-				<Route></Route>
+				<Route path="/" element={<Middleware />} />
+				<Route path="/maintenance" element={<Maintenance />} />
 			</Routes>
 		</Router>
 	</React.StrictMode>,
---
+++ src/pages/Maintenance.jsx
@@ -0,0 +1,10 @@
+const Maintenance = () => {
+	return (
+		<div id="maintenance">
+			<h1>MAINTENANCE EN COURS</h1>
+			<p>Pas d'inquiétude, je reviens trèèèèèès vite !!</p>
+		</div>
+	);
+};
+
+export default Maintenance;
