8adab27bb735f638ebc5a6592737d6465bb36bb1
gohnzy <gregory.nuzzo@gmail.com>
2024-11-27 18:40:21 +0100

    test backend launch

--- back/Database/connection.js
+++ back/Database/connection.js
@@ -1,12 +1,14 @@
 const { Pool } = require('pg');
 
+// Configurer le pool en fonction de l'environnement
 const pool = new Pool({
-	user: 'gohnzy',
-	host: 'localhost',
-	database: 'GohnZyPortfolio',
-	password: 'gohnzy1234',
-	port: 2000,
+	connectionString:
+		process.env.DATABASE_URL ||
+		'postgresql://gohnzy:gohnzy1234@localhost:2000/GohnZyPortfolio',
+	ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
 });
 
+// Fonction pour exécuter des requêtes
 const query = (text, params) => pool.query(text, params);
+
 module.exports = { query };
--- back/index.js
+++ back/index.js
@@ -1,5 +1,6 @@
 const Serveur = require('./Routes/DBConnectionRoute');
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const Projects = require('./Routes/MainRoutes');
 
@@ -9,6 +10,10 @@ const PORT = process.env.PORT || 1000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../build')));
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
 app.use('/api', Serveur);
 app.use('/api/projects', Projects);
