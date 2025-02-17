95654ecdb32c0ea5261a2313514d489b3e581104
gohnzy <gregory.nuzzo@gmail.com>
2025-02-17 12:11:44 +0100

    improves on https redirect and google referencment

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ import Gallery from './pages/Gallery';
 
 import './assets/styles/index.css';
 
+if (window.location.protocol !== 'https:') {
+	window.location.href =
+		'https://' +
+		window.location.host +
+		window.location.pathname +
+		window.location.search;
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
