b7232e26b0ac617c66200779a9db270f947f0348
gohnzy <gregory.nuzzo@gmail.com>
2025-03-03 09:20:32 +0100

    eslint erros disabled

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
